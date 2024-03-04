0bafc7bf124ab183645dee9953be1eaecc51c09c
whereyoucanfindme <74447376+vectorsigmaissomewhere@users.noreply.github.com>
2024-03-03 20:08:30 -0800

    Create views.py

---
+++ views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,redirect
+from .models import *
+from django.http import HttpResponse
+# Create your views here.
+
+def receipes(request):
+    if request.method == "POST":
+        data = request.POST
+        receipe_image = request.FILES.get('receipe_image')
+        receipe_name = data.get('receipe_name')
+        receipe_description = data.get('receipe_description')
+
+        Receipe.objects.create(
+            receipe_image = receipe_image,
+            receipe_name = receipe_name,
+            receipe_description = receipe_description,
+        )
+        """
+        print(receipe_description)
+        print(receipe_name)
+        print(receipe_image)
+        """
+        return redirect('/receipes/')
+    
+    queryset = Receipe.objects.all()
+    context = {'receipes': queryset}
+    return render(request, 'receipes.html', context)
+
+def delete_receipe(request,id):
+    queryset = Receipe.objects.get(id = id)
+    queryset.delete()
+    return redirect('/receipes/')
